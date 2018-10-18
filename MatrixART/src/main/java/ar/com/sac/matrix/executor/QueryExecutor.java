e131fcec113a7d18636f269c768061cab13793bb
cheppak <cheppi74@gmail.com>
2018-10-18 00:41:38 -0300

    Implementamos executors y sus validaciones

--- MatrixART/src/main/java/ar/com/sac/matrix/executor/QueryExecutor.java
+++ MatrixART/src/main/java/ar/com/sac/matrix/executor/QueryExecutor.java
@@ -1,21 +1,66 @@
 package ar.com.sac.matrix.executor;
 
+import org.apache.commons.lang3.StringUtils;
+
+import ar.com.sac.matrix.result.Result;
 import ar.com.sac.matrix.result.QueryResult;
 
 public class QueryExecutor extends MatrixExecutor{
 
-	public QueryResult doExecute(String script) {
-		return null;
+	public Result doExecute(String[] script, int[][][] matrix) {
+		
+		int result = 0;
+		int x1 = Integer.parseInt(script[1]);
+		int y1 = Integer.parseInt(script[2]);
+		int z1 = Integer.parseInt(script[3]);
+		int x2 = Integer.parseInt(script[4]);
+		int y2 = Integer.parseInt(script[5]);
+		int z2 = Integer.parseInt(script[6]);
+
+		for (int i = x1; i <= x2; i++) {
+			for (int j = y1; j <= y2; j++) {
+				for (int k = z1; k <= z2; k++) {
+					result = result + matrix[i][j][k];
+				}
+			}
+		}
+
+		return new QueryResult(Result.SUCCESS,result);
 	}
 
 	//	 ● 1 <= N <= 100 <br>
 	//	 ● 1 <= x1 <= x2 <= N <br>
 	//	 ● 1 <= y1 <= y2 <= N <br>
 	//	 ● 1 <= z1 <= z2 <= N <br>
-
 	@Override
-	boolean doValidate(String[] Script) {
-		// TODO Auto-generated method stub
+	boolean doValidate(String[] script, int n) {
+		if(script.length != 7)
+			return false;
+		
+		String x1 = script[1];
+		String y1 = script[2];
+		String z1 = script[3];
+		String x2 = script[4];
+		String y2 = script[5];
+		String z2 = script[6];
+
+		if(!StringUtils.isNumeric(x1) || !StringUtils.isNumeric(y1) || !StringUtils.isNumeric(z1) || !StringUtils.isNumeric(x2) || 
+				!StringUtils.isNumeric(y2)||!StringUtils.isNumeric(z2)) 
+			return false;
+		
+		int x1Value = Integer.parseInt(x1);
+		int y1Value = Integer.parseInt(y1);
+		int z1Value = Integer.parseInt(z1);
+		int x2Value = Integer.parseInt(x2);
+		int y2Value = Integer.parseInt(y2);
+		int z2Value = Integer.parseInt(z2);
+		
+		if( x1Value > n || y1Value > n || z1Value > n || x2Value > n || y2Value > n || z2Value > n)
+			return false;
+		
+		if(x1Value > x2Value || y1Value > y2Value || z1Value > z2Value)
+			return false;
+		
 		return true;
 	}
 
--- MatrixART/src/main/java/ar/com/sac/matrix/executor/UpdateExecutor.java
+++ MatrixART/src/main/java/ar/com/sac/matrix/executor/UpdateExecutor.java
@@ -1,22 +1,49 @@
 package ar.com.sac.matrix.executor;
 
-import ar.com.sac.matrix.result.QueryResult;
+import org.apache.commons.lang3.StringUtils;
+
+import ar.com.sac.matrix.result.Result;
 
 public class UpdateExecutor extends MatrixExecutor{
 
-	public QueryResult doExecute(String script) {
-		return new QueryResult(QueryResult.SUCCESS);
+	public Result doExecute(String[] script, int [][][] matrix) {
+		int x = Integer.parseInt(script[1]);
+		int y = Integer.parseInt(script[2]);
+		int z = Integer.parseInt(script[3]);
+		int value = Integer.parseInt(script[4]);
+		matrix[x][y][z] = value;
+		return new Result(Result.SUCCESS);
 	}
 
 	//  1 <= N <= 100 <br>
 	//	1 <= x,y,z <= N <br>
 	//	-126 <= W <= 126 
 	@Override
-	boolean doValidate(String[] Script) {
-		// TODO Auto-generated method stub
+	boolean doValidate(String[] script, int n) {
+
+		if(script.length != 5)
+			return false;
+		
+		String x = script[1];
+		String y = script[2];
+		String z = script[3];
+		String w = script[4];
+
+		if(!StringUtils.isNumeric(x) || !StringUtils.isNumeric(y) || !StringUtils.isNumeric(z) || !StringUtils.isNumeric(w)) 
+			return false;
+		
+		int xValue = Integer.parseInt(x);
+		int yValue = Integer.parseInt(y);
+		int zValue = Integer.parseInt(z);
+		int updateValue = Integer.parseInt(w);
+		
+		if( xValue > n || yValue > n || zValue > n)
+			return false;
+		
+		if(updateValue > 127 || updateValue < -127)
+			return false;
+		
 		return true;
 	}
-
-	
 	
 }
