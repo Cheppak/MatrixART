f8ad35ea75e3d3e2e768b8becf6f8a70db5ea8ea
cheppak <cheppi74@gmail.com>
2018-10-18 00:43:24 -0300

    Refactor en Manager. Exceptions ante problemas

--- MatrixART/src/main/java/ar/com/sac/matrix/manager/MatrixManager.java
+++ MatrixART/src/main/java/ar/com/sac/matrix/manager/MatrixManager.java
@@ -3,26 +3,30 @@ package ar.com.sac.matrix.manager;
 import ar.com.sac.matrix.executor.MatrixExecutor;
 import ar.com.sac.matrix.executor.QueryExecutor;
 import ar.com.sac.matrix.executor.UpdateExecutor;
-import ar.com.sac.matrix.result.QueryResult;
+import ar.com.sac.matrix.result.Result;
 
 public class MatrixManager {
 
-	int[][][] matrix;
-	MatrixExecutor executor;
+	public static final String INVALID_QUERY_ERROR_MSJ = "No es una instruccion valida: ";
+	public static final String DIMENSION_ERROR_MSJ = "la dimension no puede superar los 100 elementos";
+	private int[][][] matrix;
+	private MatrixExecutor executor;
 	
 	public MatrixManager(int dimensions) {
+		if(dimensions > 100) {
+			throw new RuntimeException(DIMENSION_ERROR_MSJ);
+		}
 		this.matrix = new int[dimensions][dimensions][dimensions];
 	}
 	
-	public QueryResult execute(String query) {
+	public Result execute(String query) {
 		
 		if(query.startsWith("QUERY"))
 			executor = new QueryExecutor();
 		else if(query.startsWith("UPDATE"))
 			executor = new UpdateExecutor();
-		//TODO seria mejor tal vez lanzar una exception. Ver.
-		else return new QueryResult(QueryResult.ERROR); 		
+		else throw new RuntimeException(INVALID_QUERY_ERROR_MSJ + query ); 		
 		
-		return executor.execute(query);
+		return executor.execute(query, this.matrix);
 	}
 }
